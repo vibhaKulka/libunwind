8e2b31e766dd502f6df74909e04a7dbdf5182eb1
Nikita Lapkov <nikita.lapkov@gmail.com>
2019-04-06 18:39:08 +0000

    add unw_backtrace for jemalloc

--- src/libunwind.cpp
+++ src/libunwind.cpp
@@ -278,6 +278,26 @@ void __unw_remove_dynamic_fde(unw_word_t fde) {
 #endif // defined(_LIBUNWIND_SUPPORT_DWARF_UNWIND)
 #endif // !defined(__USING_SJLJ_EXCEPTIONS__)
 
+/// Convenient helper (added for jemalloc)
+int unw_backtrace(void **buffer, int size) {
+  unw_context_t context;
+  unw_cursor_t cursor;
+  if (unw_getcontext(&context) || unw_init_local(&cursor, &context)) {
+    return 0;
+  }
+
+  unw_word_t ip;
+  int current = 0;
+  while (unw_step(&cursor) > 0) {
+    if (current >= size || unw_get_reg(&cursor, UNW_REG_IP, &ip)) {
+      break;
+    }
+
+    buffer[current++] = reinterpret_cast<void *>(static_cast<uintptr_t>(ip));
+  }
+
+  return current;
+}
 
 
 // Add logging hooks in Debug builds only
