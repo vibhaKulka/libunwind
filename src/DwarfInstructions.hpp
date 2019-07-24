36df74d804e03bf5fa4bdd2bfafde8662ad28129
Alexey Milovidov <milovidov@yandex-team.ru>
2019-07-24 03:18:57 +0300

    Mitigate segfault in libunwind #6124

--- src/DwarfInstructions.hpp
+++ src/DwarfInstructions.hpp
@@ -72,7 +72,7 @@ if (prolog.cfaRegister != 0) {
     }
 
     if (prolog.cfaExpression != 0)
-      return evaluateExpression((pint_t)prolog.cfaExpression, addressSpace, 
+      return evaluateExpression((pint_t)prolog.cfaExpression, addressSpace,
                                 registers, 0);
     assert(0 && "getCFA(): unknown location");
     __builtin_unreachable();
@@ -165,6 +165,18 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace, pint_t pc,
                                             R::getArch(), &prolog)) {
       // get pointer to cfa (architecture specific)
       pint_t cfa = getCFA(addressSpace, prolog, registers);
+
+      // Check validity of the CFA.
+      // This is a very dirty hack (inspired by original libunwind version).
+      // Motivation: sometimes libunwind parse wrong value instead of CFA.
+      // We check that memory address belongs to our process by issuing "mincore" syscall.
+      // Actually we don't care if the address is in core or not, we only check return code.
+      // If Address Sanitizer will argue, replace syscall to inline assembly.
+      {
+        unsigned char mincore_res = 0;
+        if (0 != syscall(SYS_mincore, (void*)(cfa / 4096 * 4096), 1, &mincore_res))
+          return UNW_EBADFRAME;
+      }
 
        // restore registers that DWARF says were saved
       R newRegisters = registers;
